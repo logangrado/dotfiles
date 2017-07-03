3ef7d56a9a47b70799042dabe3bef1d865351444
Logan Grado <logangrado@gmail.com>
2017-07-03 09:50:26 -0400

    added install script, gitignore, and tmux.conf

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DOT_DIR=$PWD
+DATE_TIME=$(date '+%Y-%m-%d_%H:%M:%S')
+BACKUP_DIR="$DOT_DIR/backups/$DATE_TIME"
+
+FILES="zshrc tmux.conf"
+
+# create dotfiles.bak
+echo "Making backup directory: $BACKUP_DIR"
+mkdir -p $BACKUP_DIR
+
+for f in $FILES; do
+    echo "  backing up $f"
+    mv ~/.$f $BACKUP_DIR/
+    echo "  symlinking $f from $DOT_DIR to ~/"
+    ln -s $DOT_DIR/.$f ~/.$f
+done
+echo "Done"
