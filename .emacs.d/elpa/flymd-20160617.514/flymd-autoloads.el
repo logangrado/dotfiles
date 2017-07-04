83e975f3d9e40b37c7cf960d52ffbae373dc433f
Logan Grado <Grado@Logans-MacBook-Pro.local>
2017-07-03 22:28:27 -0400

    removed elpa dir

--- .emacs.d/elpa/flymd-20160617.514/flymd-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; flymd-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "flymd" "../../../../.emacs.d/elpa/flymd-20160617.514/flymd.el"
-;;;;;;  "b5c06e6c2be564e38585950b2f51f225")
-;;; Generated autoloads from ../../../../.emacs.d/elpa/flymd-20160617.514/flymd.el
-
-(autoload 'flymd-flyit "flymd" "\
-Enable realtime markdown preview on the current buffer.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("../../../../.emacs.d/elpa/flymd-20160617.514/flymd-autoloads.el"
-;;;;;;  "../../../../.emacs.d/elpa/flymd-20160617.514/flymd-pkg.el"
-;;;;;;  "../../../../.emacs.d/elpa/flymd-20160617.514/flymd.el")
-;;;;;;  (22874 56777 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; flymd-autoloads.el ends here
