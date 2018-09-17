1c897cc44510bc0b8bc34d314b4f070cd6d1fe2a
Logan Grado <grado@umn.edu>
2018-09-17 13:55:57 -0500

    Added reverse_log.py

---
+++ scripts/reverse_log.py
@@ -0,0 +1,33 @@
+#!/usr/local/bin/python3
+import sys
+import fnmatch
+import re
+
+def reverse_log():
+    lines = []
+    for line in sys.stdin:
+        line = re.sub(r'/(?![a-zA-Z0-9:])', '<->', line) #Need to avoid matching origin/master, etc
+        line = re.sub(r'_(?![a-zA-Z0-9:])', '‾', line) #Need to avoid matching origin/master, etc
+        #line = line.replace('/', '<->')
+        line = line.replace('\\', '/')
+        line = line.replace('<->', '\\')
+
+        l2 = '*|[m[??m\\*'
+        l1 = '*|[m[??m/*'
+
+        #if len(lines)>1 and fnmatch.fnmatch(lines[-1],l1) and fnmatch.fnmatch(line,l2):
+        #    lines.pop()
+        #else:
+
+        if line[-1] != '\n':
+            line += '\n'
+        
+        lines.append(line)
+
+    for line in lines[::-1]:
+        print(line,end="")
+
+if __name__ == '__main__':
+    reverse_log()
+        
+# less +G -iXFR
