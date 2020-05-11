9828b06c3635ee857cf27fa8706c69da88f460a4
Logan Grado <lgrado@apple.com>
2020-05-11 15:47:13 -0700

    Working daily cleanup w/ catalina, plus logging

--- cronjobs/daily_cleanup.sh
+++ cronjobs/daily_cleanup.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# Logging. Sets max size, and keeps a single backup
+LOG_DIR=~/.dotfiles/cronjobs
+LOG_FILE=$LOG_DIR/daily_cleanup.log
+LOG_SIZE=0
+if [ -f $LOG_FILE ]; then
+    LOG_SIZE=$(wc -c $LOG_FILE)
+fi
+MAX_SIZE=$((8*1024*1024)) # Max size 1MB
+if [ $LOG_SIZE -ge $MAX_SIZE ]; then
+    mv $LOG_FILE $LOG_FILE.1
+    rm $LOG_FILE
+fi
+
+echo "[$(DATE)] Running daily cleanup" >> $LOG_FILE
+
 #Check for folder existence, create if none
 if [ ! -d ~/Downloads/Today ]; then
     mkdir ~/Downloads/Today
@@ -10,6 +25,9 @@ fi
 if [ ! -d ~/Downloads/Last\ Week ]; then
     mkdir ~/Downloads/Last\ Week
 fi
+if [ ! -d ~/Downloads/Last\ Month ]; then
+    mkdir ~/Downloads/Last\ Month
+fi
 
 # Empty trash
 rm -rf ~/.Trash/*
@@ -18,4 +36,7 @@ rm -rf ~/.Trash/*
 # Meant to be run at 4AM
 find ~/Downloads/Today/* -Btime +4h -exec mv "{}" ~/Downloads/Yesterday/ \;
 find ~/Downloads/Yesterday/* -Btime +1d4h -exec mv "{}" ~/Downloads/Last\ Week/ \;
-find ~/Downloads/Last\ Week/* -Btime +7d -exec rm -rf "{}" \;
+find ~/Downloads/Last\ Week/* -Btime +7d -exec  mv "{}" ~/Downloads/Last\ Month/ \;
+find ~/Downloads/Last\ Week/* -Btime +30d -exec rm -rf "{}" \;
+
+# NOTE: On catalina, you have to give cron full disk access. Drag /usr/sbin/cron into System Preferences -> Security & Privacy -> Privacy tab
