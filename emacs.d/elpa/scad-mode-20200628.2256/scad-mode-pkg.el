48c61c98d9d65d84c56e1d2c8e34fbdef599b798
Logan Grado <logangrado@gmail.com>
2020-11-19 14:35:43 -0800

    emacs: removed elpa

--- emacs.d/elpa/scad-mode-20200628.2256/scad-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "scad-mode" "20200628.2256" "A major mode for editing OpenSCAD code" 'nil :commit "67fafca8734b97a8ce509b754a54bade968cce42" :keywords '("languages") :authors '(("Len Trigg, Łukasz Stelmach")) :maintainer '("Len Trigg" . "lenbok@gmail.com") :url "https://raw.github.com/openscad/openscad/master/contrib/scad-mode.el")
--- emacs.d/elpa/web-mode-20200820.1325/web-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "web-mode" "20200820.1325" "major mode for editing web templates" '((emacs "23.1")) :commit "3ecfe6b7f508556e935788e40784f254f8334837" :keywords '("languages") :authors '(("François-Xavier Bois <fxbois AT Google Mail Service>")) :maintainer '("François-Xavier Bois") :url "http://web-mode.org")
