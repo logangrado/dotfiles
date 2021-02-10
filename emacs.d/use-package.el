6da9283011ee318ee517cd8a19183e8c0b780674
Logan Grado <logangrado@gmail.com>
2021-02-10 13:17:21 -0800

    emacs: added protobuf mode

--- emacs.d/use-package.el
+++ emacs.d/use-package.el
@@ -1,3 +1,14 @@
+(use-package protobuf-mode
+  :ensure t
+  :init
+  (defconst my-protobuf-style
+    '((c-basic-offset . 2)
+      (indent-tabs-mode . nil)))
+  
+  (add-hook 'protobuf-mode-hook
+            (lambda () (c-add-style "my-style" my-protobuf-style t)))
+  )
+  
 (use-package jsonnet-mode
   :ensure t
   )
