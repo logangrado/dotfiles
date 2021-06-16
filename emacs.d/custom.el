2cfffd2c8e326551e1bc1fefaddfd029818bf379
Logan Grado <logangrado@gmail.com>
2021-06-16 11:16:36 -0700

    custom.el: Update

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -4,15 +4,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   (quote
-    ("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+   '("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))
  '(doc-view-continuous t)
  '(neo-hidden-regexp-list
-   (quote
-    ("\\.pyc\\'" ".*.pyc" "^\\." "\\.pyc$" "~$" "^#.*#$" "\\.elc$")))
+   '("\\.pyc\\'" ".*.pyc" "^\\." "\\.pyc$" "~$" "^#.*#$" "\\.elc$"))
  '(org-agenda-custom-commands
-   (quote
-    (("d" todo "DELEGATED" nil)
+   '(("d" todo "DELEGATED" nil)
      ("c" todo "DONE|DEFERRED|CANCELLED" nil)
      ("w" todo "WAITING" nil)
      ("W" agenda ""
@@ -20,24 +17,16 @@
      ("A" agenda ""
       ((org-agenda-skip-function
         (lambda nil
-          (org-agenda-skip-entry-if
-           (quote notregexp)
-           "\\=.*\\[#A\\]")))
+          (org-agenda-skip-entry-if 'notregexp "\\=.*\\[#A\\]")))
        (org-agenda-ndays 1)
        (org-agenda-overriding-header "Today's Priority #A tasks: ")))
      ("u" alltodo ""
       ((org-agenda-skip-function
         (lambda nil
-          (org-agenda-skip-entry-if
-           (quote scheduled)
-           (quote deadline)
-           (quote regexp)
-           "
+          (org-agenda-skip-entry-if 'scheduled 'deadline 'regexp "
 ]+>")))
-       (org-agenda-overriding-header "Unscheduled TODO entries: "))))))
- '(org-agenda-files
-   (quote
-    ("~/org/notes.org" "~/org/todo.org ~/org/notes.org")))
+       (org-agenda-overriding-header "Unscheduled TODO entries: ")))))
+ '(org-agenda-files '("~/org/notes.org" "~/org/todo.org ~/org/notes.org"))
  '(org-agenda-ndays 7 t)
  '(org-agenda-show-all-dates t t)
  '(org-agenda-skip-deadline-if-done t t)
@@ -45,19 +34,17 @@
  '(org-agenda-start-on-weekday nil t)
  '(org-deadline-warning-days 14)
  '(org-default-notes-file "~/notes.org")
- '(org-fast-tag-selection-single-key (quote expert))
+ '(org-fast-tag-selection-single-key 'expert)
  '(org-remember-store-without-prompt t)
  '(org-remember-templates
-   (quote
-    ((116 "* TODO %?
+   '((116 "* TODO %?
   %u" "~/todo.org" "Tasks")
-     (110 "* %u %?" "~/notes.org" "Notes"))))
+     (110 "* %u %?" "~/notes.org" "Notes")))
  '(org-reverse-note-order t)
  '(package-selected-packages
-   (quote
-    (log4j-mode Log4j jsonnet-mode yaml-mode csv-mode flymd tabbar js2-mode iedit i-edit undo-tree real-auto-save exec-path-from-shell latex-preview-pane list-packages-ext org-bullets synonymous magit doom-themes-org doom-themes outline-magic scad-mode auto-complete outshine pretty-mode which-key prettify-greek org org-mode matlab-mode web-mode use-package pbcopy nlinum neotree markdown-mode auctex adaptive-wrap 0blayout)))
- '(remember-annotation-functions (quote (org-remember-annotation)))
- '(remember-handler-functions (quote (org-remember-handler))))
+   '(ibuffer-vc projectile frame-purpose pretty-hydra hydra lv f dash-functional quelpa-use-package quelpa bufler protobuf-mode log4j-mode Log4j jsonnet-mode yaml-mode csv-mode flymd tabbar js2-mode iedit i-edit undo-tree real-auto-save exec-path-from-shell latex-preview-pane list-packages-ext org-bullets synonymous magit doom-themes-org doom-themes outline-magic scad-mode auto-complete outshine pretty-mode which-key prettify-greek org org-mode matlab-mode web-mode use-package pbcopy nlinum neotree markdown-mode auctex adaptive-wrap 0blayout))
+ '(remember-annotation-functions '(org-remember-annotation))
+ '(remember-handler-functions '(org-remember-handler)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
