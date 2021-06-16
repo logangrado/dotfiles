2b9a2cb89f184b23508708997c8b8ab3f08a9c7e
Logan Grado <logangrado@gmail.com>
2021-06-16 11:18:50 -0700

    reverse_log: updated

--- scripts/reverse_log.py
+++ scripts/reverse_log.py
@@ -4,31 +4,35 @@ import sys
 import fnmatch
 import re
 
+
 def reverse_log():
     lines = []
     for line in sys.stdin:
-        line = re.sub(r'/(?![a-zA-Z0-9:])', '<->', line) #Need to avoid matching origin/master, etc
-        line = re.sub(r'_(?![a-zA-Z0-9:])', '‾', line) #Need to avoid matching origin/master, etc
-        #line = line.replace('/', '<->')
-        line = line.replace('\\', '/')
-        line = line.replace('<->', '\\')
+        line = re.sub(r"/(?![a-zA-Z0-9:])", "<->", line)  # Need to avoid matching origin/master, etc
+        line = re.sub(r"_(?![a-zA-Z0-9:])", "‾", line)  # Need to avoid matching origin/master, etc
+        # line = line.replace('/', '<->')
+        line = line.replace("\\", "/")
+        line = line.replace("<->", "\\")
 
-        l2 = '*|[m[??m\\*'
-        l1 = '*|[m[??m/*'
+        l2 = "*|[m[??m\\*"
+        l1 = "*|[m[??m/*"
 
-        #if len(lines)>1 and fnmatch.fnmatch(lines[-1],l1) and fnmatch.fnmatch(line,l2):
+        # if len(lines)>1 and fnmatch.fnmatch(lines[-1],l1) and fnmatch.fnmatch(line,l2):
         #    lines.pop()
-        #else:
+        # else:
 
-        if line[-1] != '\n':
-            line += '\n'
-        
+        if line[-1] != "\n":
+            line += "\n"
+
         lines.append(line)
 
-    for line in lines[::-1]:
-        print(line,end="")
+    print("".join(lines[::-1]))
+    # for line in lines[::-1]:
+    #    print(line,end="")
+    # print(end="")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     reverse_log()
-        
+
 # less +G -iXFR
