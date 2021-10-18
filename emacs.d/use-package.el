d7dae2bce5baa5ccce73609636236486fe7826e4
Logan Grado <logangrado@gmail.com>
2021-10-18 11:07:00 -0700

    CompanyMode: Use TAB for complete

--- emacs.d/use-package.el
+++ emacs.d/use-package.el
@@ -5,8 +5,14 @@
   )
 
 (use-package company
+  ;; Complete Anything (company). Autocomplete popup for code.
+  ;; Distinct from ivy, which auto-completes buffer commands
   :ensure t
+  :bind
+  (:map company-active-map ("RET" . newline))  ;; Make RET enter a newline
+  (:map company-active-map ("TAB" . company-complete-selection)) ;; Use TAB for completion (instead of complete common)  
   )
+
 
 (use-package elpy
   :ensure t
