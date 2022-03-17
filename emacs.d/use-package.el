307986a3af29917bf78d5931976558ebff466592
Logan Grado <logangrado@gmail.com>
2022-03-17 08:54:02 -0700

    use-package: Add org-roam

--- emacs.d/use-package.el
+++ emacs.d/use-package.el
@@ -1,4 +1,22 @@
 ;; Inbox
+(use-package org-roam
+  ;;:ensure t
+  :after org
+  :init
+  (setq org-roam-v2-ack t) ;; Acknowledge V2 upgrade
+  :custom
+  (org-roam-directory "~/org/roam")
+  :config
+  (org-roam-setup)
+  :bind (("C-c n f" . org-roam-node-find)
+           ("C-c n r" . org-roam-node-random)		    
+           (:map org-mode-map
+                 (("C-c n i" . org-roam-node-insert)
+                  ("C-c n o" . org-id-get-create)
+                  ("C-c n t" . org-roam-tag-add)
+                  ("C-c n a" . org-roam-alias-add)
+                  ("C-c n l" . org-roam-buffer-toggle))))
+  )
 
 ;; Python Related
 ;;================================================================
