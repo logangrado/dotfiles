278b9395fac2f407f89ab135a8f2689f99c8b8a6
Logan Grado <logangrado@gmail.com>
2023-02-22 14:36:21 -0800

    doom.config: Always display workspace tab bar on bottom

--- doom.d/config.el
+++ doom.d/config.el
@@ -74,3 +74,12 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+;;=================================================================
+;; Always display workspace tab bar on bottom
+(after! persp-mode
+  (defun display-workspaces-in-minibuffer ()
+    (with-current-buffer " *Minibuf-0*"
+      (erase-buffer)
+      (insert (+workspace--tabline))))
+  (run-with-idle-timer 1 t #'display-workspaces-in-minibuffer)
+  (+workspace/display))
