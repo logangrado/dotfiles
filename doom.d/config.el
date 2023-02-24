5278d15d786d888afb9f688d6c1b75dc9eecf83e
Logan Grado <logangrado@gmail.com>
2023-02-24 11:26:58 -0800

    doom: Origami code folding!

--- doom.d/config.el
+++ doom.d/config.el
@@ -70,3 +70,20 @@
 (use-package! auto-dim-other-buffers
   :init
   (auto-dim-other-buffers-mode))
+
+(use-package!  origami
+  :hook python-mode
+  :init
+  (origami-mode)
+  :config
+  (map! :map evil-normal-state-map
+        :prefix ("z" . "origami")
+        "j" #'origami-forward-fold  ;;origami-next-fold
+        "k" #'origami-previous-fold  ;;origami-previous-fold
+        "o" #'origami-open-node-recursively
+        "O" #'origami-open-all-nodeso
+        "c" #'origami-close-node-recursively
+        "C" #'origami-close-all-nodes
+        "t" #'origami-recursively-toggle-node
+        "T" #'origami-toggle-all-nodes)
+ )
