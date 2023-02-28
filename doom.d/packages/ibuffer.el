f7d381eddcc17c345f84c02df36b4236d734ecb9
Logan Grado <lgrado@apple.com>
2023-02-27 22:30:43 -0800

    magit color config, ibuffer expert mode

--- doom.d/packages/ibuffer.el
+++ doom.d/packages/ibuffer.el
@@ -3,4 +3,6 @@
 (after! ibuffer
   ;; Don't show magit buffers in ibuffer
   (add-to-list 'ibuffer-never-show-predicates "magit*")
+  ;; Don't ask for confirmation when killing buffers
+  (setq ibuffer-expert t)
   )
---
+++ doom.d/packages/magit.el
@@ -0,0 +1,49 @@
+;;; ../.dotfiles/doom.d/packages/magit.el -*- lexical-binding: t; -*-
+
+
+(use-package magit
+  :config
+  (setq magit-diff-refine-hunk t)
+
+  (custom-set-faces!
+    `(magit-branch-local :foreground ,(doom-color 'blue) :bold t)
+    `(magit-branch-current :inherit magit-branch-local :underline t)
+    `(magit-branch-remote :foreground ,(doom-color 'red) :bold t)
+    `(magit-branch-remote-head :inherit magit-branch-remote :box nil :underline t)
+    )
+
+  ;; Colors!
+  ;; Branches and tags
+  ;; (set-face-attribute 'magit-branch-local nil :inherit 'ansi-color-green :bold t)
+  ;; (set-face-attribute 'magit-branch-remote nil :foreground "red" :bold t)
+  ;; (set-face-attribute 'magit-branch-current nil :foreground "cyan" :bold t)
+  ;; (set-face-attribute 'magit-tag nil :foreground "cyan")
+
+  ;; ;; Section headers
+  ;; (set-face-attribute 'magit-section-heading nil :foreground "yellow")
+  ;; (set-face-attribute 'magit-section-highlight nil :background "black")
+
+  ;; ;; Diff-ing
+  ;; (set-face-attribute 'magit-diff-hunk-heading-highlight nil :foreground "grey70" :background "grey35")
+  ;; (set-face-attribute 'magit-diff-hunk-heading nil :foreground "grey70" :background "grey25")
+
+  ;; (set-face-attribute 'magit-diff-context nil :foreground "brightblue" :background "brightblack")
+  ;; (set-face-attribute 'magit-diff-context-highlight nil :foreground "grey70" :background "grey20")
+
+  ;; (set-face-attribute 'magit-diff-removed t :foreground "red" :background "brightblack" :inherit 'magit-diff-context)
+  ;; (set-face-attribute 'magit-diff-removed-highlight t :foreground "red" :background "grey20" :inherit 'magit-diff-context-highlight)
+
+  ;; (set-face-attribute 'magit-diff-added nil :foreground "green" :background "brightblack" :inherit 'magit-diff-context)
+  ;; (set-face-attribute 'magit-diff-added-highlight nil :foreground "green" :background "grey20" :inherit 'magit-diff-context-highlight)
+
+
+  ;; ;; (set-face-attribute 'diff-refine-added nil :foreground "grey70" :background "color-29" :inverse-video nil)
+  ;; ;; (set-face-attribute 'diff-refine-removed nil :foreground "grey70" :background "color-88" :inverse-video nil)
+
+  ;; ;; (set-face-attribute 'diff-refine-added nil :foreground "grey10" :background "color-23" :inverse-video nil)
+  ;; ;; (set-face-attribute 'diff-refine-removed nil :foreground "grey5" :background "color-52" :inverse-video nil)
+
+
+  ;; ;; Display the status buffer in full-frame mode
+  ;; (setq magit-display-buffer-function 'magit-display-buffer-fullframe-status-v1)
+  )
