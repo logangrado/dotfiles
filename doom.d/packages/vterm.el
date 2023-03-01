b00fd573302688aaf702b016a2a38325590da468
Logan Grado <lgrado@apple.com>
2023-02-28 20:26:19 -0800

    Working on vterm toggle

--- doom.d/packages/vterm.el
+++ doom.d/packages/vterm.el
@@ -4,4 +4,19 @@
 (use-package! vterm
   :config
   (setq vterm-shell "zsh")
-  )
+  ;;(set-popup-rule! "^vterm" :size 0.25 :vslot -4 :select t :quit nil :ttl 0)
+  ;;(set-popup-rule! "*doom:vterm-popup:main" :size 0.25 :vslot -4 :select t :quit nil :ttl 0 :side 'bottom)
+  (defun my-vterm/split-bottom ()
+    "Create a vterm window below the current one"
+    ;; Now we just got to figure out how to make this a "toggle" popup
+    ;; I think we'd have to create a "named" vterm window
+    (interactive)
+    (let* ((ignore-window-parameters t)
+           (dedicated-p (window-dedicated-p)))
+      (split-window-vertically (floor (* 0.70 (window-height))))
+      (other-window 1)
+      (+vterm/here default-directory)))
+
+  ;; MIGHT BE WORTH COPY/PASTING +vterm/toggle's SOURCE CODE AND WORKING FROM THERE?
+  ;; it seems like vterm/toggle creates a _named_ vterm buffer, which is what we need to do
+ )
