83a904c27bef4a61b14ef770cfb6ac46bc0b7c8e
Logan Grado <logangrado@gmail.com>
2023-03-03 14:51:29 -0800

    doom: Add disable-mouse package

--- doom.d/packages.el
+++ doom.d/packages.el
@@ -16,6 +16,7 @@
 (package! origami)
 (package! drag-stuff)
 (package! auto-dim-other-buffers)
+(package! disable-mouse)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
