d281b6efea566f04bc20b15b8d7cde7d54525855
Logan Grado <lgrado@apple.com>
2023-03-05 10:16:24 -0800

    python-black. Already handled by format-all

--- doom.d/packages.el
+++ doom.d/packages.el
@@ -17,6 +17,7 @@
 (package! drag-stuff)
 (package! auto-dim-other-buffers)
 (package! disable-mouse)
+(package! python-black)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
