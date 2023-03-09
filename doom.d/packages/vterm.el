03f6ca911a4278060633ee4c7b4be91cdd36d5ad
Logan Grado <logangrado@gmail.com>
2023-03-09 13:34:52 -0800

    vterm: open terminal here names by workspace

--- doom.d/packages/vterm.el
+++ doom.d/packages/vterm.el
@@ -2,9 +2,38 @@
 
 
 (use-package! vterm
+  :init
+  (defun +vterm/get-workspace-buffer-name ()
+    "Retrieve vterm buffer name for this workspace"
+    (format "*vterm:%s*"
+            (if
+                (bound-and-true-p persp-mode)
+                (safe-persp-name (get-current-persp))
+              "main"))
+
+    )
+
+  (defun +vterm/here-workspace (arg)
+    "Open a terminal buffer in the current window at project root.
+
+  If prefix ARG is non-nil, cd into `default-directory' instead of project root.
+
+  Returns the vterm buffer."
+    (interactive "P")
+    (+vterm--configure-project-root-and-display
+     arg
+     (lambda()
+       (require 'vterm)
+       ;; HACK forces vterm to redraw, fixing strange artefacting in the tty.
+       (save-window-excursion
+         (pop-to-buffer "*scratch*"))
+       (let (display-buffer-alist)
+         (vterm (+vterm/get-workspace-buffer-name))))))
+
   :config
   (setq vterm-shell "zsh")
   ;; Define keys in vterm-mode-map, active in normal-state
+
   (map! :map vterm-mode-map
         "<normal-state> '" #'(lambda()(interactive) (vterm-send-key "<up>"))
         "<normal-state> \"" #'(lambda()(interactive) (vterm-send-key "<down>"))
@@ -18,5 +47,9 @@
         ;; "<normal-state> \"" #'vterm-send-down
         ;; "<normal-state> c c" #'vterm-send-C-c
         ;; "<normal-state> c d" #'vterm-send-C-d
+        )
+
+  (map! :leader
+        "o T" #'+vterm/here-workspace
         )
   )
