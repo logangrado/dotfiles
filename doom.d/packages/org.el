e8ddd784de3919c2f7ad4e3e1dd2fa0a11d861b2
Logan Grado <logangrado@gmail.com>
2023-03-20 14:44:48 -0700

    Working on org faces

--- doom.d/packages/org.el
+++ doom.d/packages/org.el
@@ -1,7 +1,6 @@
 ;;; ../.dotfiles/doom.d/packages/org.el -*- lexical-binding: t; -*-
 
 (after! org
-  )
   ;; (setq org-todo-keywords
   ;;       '((sequence "TODO(t)" "PROG(p)" "|" "DONE(d)" "CANCELED(c)")))
 
@@ -29,58 +28,59 @@
   ;;        ("PROG" . +org-todo-cancel)
   ;;        )
   ;;   )
-  ;; (cu;; stom-set-faces!
-  ;;   `(org-todo :foreground ,(nth 2 (doom-themes--colors-p 'red)))
-  ;;   )
+  (custom-set-faces!
+    `(org-todo :foreground ,(nth 2 (doom-themes--colors-p 'red)) :box t)
+    )
 
-  ;; (with-no-warnings
-  ;;   (custom-declare-face '+org-todo-active  '((t (:inherit (bold font-lock-constant-face org-todo)))) "")
-  ;;   (custom-declare-face '+org-todo-project '((t (:inherit (bold font-lock-doc-face org-todo)))) "")
-  ;;   (custom-declare-face '+org-todo-onhold  '((t (:inherit (bold warning org-todo)))) "")
-  ;;   (custom-declare-face '+org-todo-cancel  '((t (:inherit (bold error org-todo)))) ""))
+  (with-no-warnings
+    (custom-declare-face '+org-todo-active  '((t (:inherit (bold font-lock-constant-face org-todo)))) "")
+    (custom-declare-face '+org-todo-project '((t (:inherit (bold font-lock-doc-face org-todo)))) "")
+    (custom-declare-face '+org-todo-onhold  '((t (:inherit (bold warning org-todo)))) "")
+    (custom-declare-face '+org-todo-cancel  '((t (:inherit (bold error org-todo)))) ""))
 
-  ;; (setq org-todo-keywords
-  ;;       '((sequence
-  ;;          "TODO(t)"  ; A task that needs doing & is ready to do
-  ;;          "PROJ(p)"  ; A project, which usually contains other tasks
-  ;;          "LOOP(r)"  ; A recurring task
-  ;;          "STRT(s)"  ; A task that is in progress
-  ;;          "WAIT(w)"  ; Something external is holding up this task
-  ;;          "HOLD(h)"  ; This task is paused/on hold because of me
-  ;;          "IDEA(i)"  ; An unconfirmed and unapproved task or notion
-  ;;          "|"
-  ;;          "DONE(d)"  ; Task successfully completed
-  ;;          "KILL(k)") ; Task was cancelled, aborted or is no longer applicable
-  ;;         (sequence
-  ;;          "[ ](T)"   ; A task that needs doing
-  ;;          "[-](S)"   ; Task is in progress
-  ;;          "[?](W)"   ; Task is being held up or paused
-  ;;          "|"
-  ;;          "[X](D)")  ; Task was completed
-  ;;         (sequence
-  ;;          "|"
-  ;;          "OKAY(o)"
-  ;;          "YES(y)"
-  ;;          "NO(n)"))
-  ;;       org-todo-keyword-faces
-  ;;       '(("[-]"  . +org-todo-active)
-  ;;         ("STRT" . +org-todo-active)
-  ;;         ("[?]"  . +org-todo-onhold)
-  ;;         ("WAIT" . +org-todo-onhold)
-  ;;         ("HOLD" . +org-todo-onhold)
-  ;;         ("PROJ" . +org-todo-project)
-  ;;         ("NO"   . +org-todo-cancel)
-  ;;         ("KILL" . +org-todo-cancel)))
+  (custom-set-faces!
+   `(+org-todo-active :foreground ,(nth 2 (doom-themes--colors-p 'yellow)))
+   `(+org-todo-project :foreground ,(nth 2 (doom-themes--colors-p 'blue)))
+   `(+org-todo-onhold :foreground ,(nth 2 (doom-themes--colors-p 'magenta)))
+   `(+org-todo-cancel :foreground ,(nth 2 (doom-themes--colors-p 'red)))
+    )
 
-  ;; (setq org-tags-column -80)
-  ;; (add-hook 'focus-in-hook
-  ;;   (lambda () (progn
-  ;;     (setq org-tags-column (- 5 (window-body-width)))) (org-align-all-tags)))
+  (setq org-todo-keywords
+        '((sequence
+           "TODO(t)"  ; A task that needs doing & is ready to do
+           "PROJ(p)"  ; A project, which usually contains other tasks
+           "LOOP(r)"  ; A recurring task
+           "STRT(s)"  ; A task that is in progress
+           "WAIT(w)"  ; Something external is holding up this task
+           "HOLD(h)"  ; This task is paused/on hold because of me
+           "IDEA(i)"  ; An unconfirmed and unapproved task or notion
+           "|"
+           "DONE(d)"  ; Task successfully completed
+           "KILL(k)") ; Task was cancelled, aborted or is no longer applicable
+          (sequence
+           "[ ](T)"   ; A task that needs doing
+           "[-](S)"   ; Task is in progress
+           "[?](W)"   ; Task is being held up or paused
+           "|"
+           "[X](D)")  ; Task was completed
+          (sequence
+           "|"
+           "OKAY(o)"
+           "YES(y)"
+           "NO(n)"))
+        org-todo-keyword-faces
+        '(("[-]"  . +org-todo-active)
+          ("STRT" . +org-todo-active)
+          ("[?]"  . +org-todo-onhold)
+          ("WAIT" . +org-todo-onhold)
+          ("HOLD" . +org-todo-onhold)
+          ("PROJ" . +org-todo-project)
+          ("NO"   . +org-todo-cancel)
+          ("KILL" . +org-todo-cancel)))
 
-  ;; (add-hook 'focus-out-hook
-  ;;   (lambda () (progn
-  ;;     (setq org-tags-column (- 5 (window-body-width)))) (org-align-all-tags)))
+  (setq org-tags-column -80)
 
+  )
 ;; (use-package! org
 ;;   ;; :bind
 ;;   ;; ("s-ret" . org-insert-heading-respect-content)
