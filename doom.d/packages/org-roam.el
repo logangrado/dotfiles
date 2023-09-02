5883d426888f588b70ddfa97cbb18288f33cd757
Logan Grado <lgrado@apple.com>
2023-09-02 10:29:55 -0700

    Org roam daily config

--- doom.d/packages/org-roam.el
+++ doom.d/packages/org-roam.el
@@ -7,14 +7,27 @@
   (setq org-roam-v2-ack t) ;; Acknowledge V2 upgrade
   :custom
   (org-roam-directory "~/org/roam")
+  (org-roam-completion-everywhere t)
+  ;; More detail in default catpure template
+  (org-roam-dailies-capture-templates
+    '(("d" "default" entry "* %<%I:%M %p>: %?"
+       :if-new (file+head "%<%Y-%m-%d>.org" "#+title: %<%Y-%m-%d>\n"))))
+  :bind
+  (("C-c n f" . org-roam-node-find)
+   ("C-c n r" . org-roam-node-random)
+   :map org-mode-map
+   ("C-c n i" . org-roam-node-insert)
+   ("C-c n o" . org-id-get-create)
+   ("C-c n t" . org-roam-tag-add)
+   ("C-c n a" . org-roam-alias-add)
+   ("C-c n l" . org-roam-buffer-toggle)
+   :map org-roam-dailies-map
+   ("Y" . org-roam-dailies-capture-yesterday)
+   ("T" . org-roam-dailies-capture-tomorrow))
+  :bind-keymap
+  ("C-c n d" . org-roam-dailies-map)
   :config
   (org-roam-setup)
-  :bind (("C-c n f" . org-roam-node-find)
-           ("C-c n r" . org-roam-node-random)
-           (:map org-mode-map
-                 (("C-c n i" . org-roam-node-insert)
-                  ("C-c n o" . org-id-get-create)
-                  ("C-c n t" . org-roam-tag-add)
-                  ("C-c n a" . org-roam-alias-add)
-                  ("C-c n l" . org-roam-buffer-toggle))))
+  (require 'org-roam-dailies) ;; Ensure the keymap is available
+  (org-roam-db-autosync-mode)
   )
