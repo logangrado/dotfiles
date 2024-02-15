e7945995e5d2e8195405f5825f6b2c0d845dc315
Logan Grado <lgrado@apple.com>
2024-02-14 17:31:45 -0800

    config.el: add zoom frame in/out

--- doom.d/config.el
+++ doom.d/config.el
@@ -191,6 +191,33 @@
      (file-notify-rm-watch key))
    file-notify-descriptors))
 
+;; Resize the whole frame, and not only a window
+;; Adapted from https://stackoverflow.com/a/24714383/5103881
+(defun acg/zoom-frame (&optional amt frame)
+  "Increaze FRAME font size by amount AMT. Defaults to selected
+frame if FRAME is nil, and to 1 if AMT is nil."
+  (interactive "p")
+  (let* ((frame (or frame (selected-frame)))
+         (font (face-attribute 'default :font frame))
+         (size (font-get font :size))
+         (amt (or amt 1))
+         (new-size (+ size amt)))
+    (set-frame-font (font-spec :size new-size) t `(,frame))
+    (message "Frame's font new size: %d" new-size)))
+
+(defun acg/zoom-frame-out (&optional amt frame)
+  "Call `acg/zoom-frame' with negative argument."
+  (interactive "p")
+  (acg/zoom-frame (- (or amt 1)) frame))
+
+(global-set-key (kbd "C-x C-=") 'acg/zoom-frame)
+(global-set-key (kbd "C-x C--") 'acg/zoom-frame-out)
+;; Move to new window on creation
+(map! :leader
+      "=" #'acg/zoom-frame
+      "-" #'acg/zoom-frame-out
+      )
+
 ;; EXTERNAL PACAKGE CONFIG
 ;;=================================================================
 ;; Load all files in packages/
