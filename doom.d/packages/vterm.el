b333328397c0ba945967e0eb12ec4070b5f24de6
Logan Grado <logan.grado@amd.com>
2024-03-14 09:19:54 -0700

    vterm: better handling of vterm buffers

--- doom.d/packages/vterm.el
+++ doom.d/packages/vterm.el
@@ -5,7 +5,7 @@
   :init
   (defun +vterm/get-workspace-buffer-name ()
     "Retrieve vterm buffer name for this workspace"
-    (format "*vterm:%s*"
+    (format "*vterm:%s"
             (if
                 (bound-and-true-p persp-mode)
                 (safe-persp-name (get-current-persp))
@@ -15,10 +15,8 @@
 
   (defun +vterm/here-workspace (arg)
     "Open a terminal buffer in the current window at project root.
-
-  If prefix ARG is non-nil, cd into `default-directory' instead of project root.
-
-  Returns the vterm buffer."
+     If prefix ARG is non-nil, cd into `default-directory' instead of project root.
+     Returns the vterm buffer."
     (interactive "P")
     (+vterm--configure-project-root-and-display
      arg
@@ -30,6 +28,35 @@
        (let (display-buffer-alist)
          (vterm (+vterm/get-workspace-buffer-name))))))
 
+  (defun +vterm/get-or-create-for-workspace ()
+    "Switch to an existing vterm buffer for the current workspace, or create one if none exists."
+    (interactive)
+    (let ((workspace-vterm-buffer-name (+vterm/get-workspace-buffer-name)))
+      ;; Check if the workspace-specific vterm buffer already exists
+      (if (get-buffer workspace-vterm-buffer-name)
+          ;; If it exists, switch to the vterm buffer
+          (switch-to-buffer workspace-vterm-buffer-name)
+        ;; If it does not exist, call +vterm/here-workspace to create it
+        (+vterm/here-workspace nil))))
+
+  (defun +vterm/toggle-window ()
+    "Switch to an existing vterm buffer below the current window, or create one
+     if none exists. Do nothing if in a vterm buffer."
+    (interactive)
+    (unless (eq major-mode 'vterm-mode) ;; Check if current window is vterm. IF not, proceed
+      (let ((window-below (window-in-direction 'below))) ;; Get the window below the current one
+        (if (and window-below ;; If there is a window below
+                 (with-current-buffer (window-buffer window-below) ;; Check if the buffer is a vterm buffer
+                   (eq major-mode 'vterm-mode)))
+            (select-window window-below) ;; If it's a vterm buffer, switch to it
+          ;; Else, create a new vterm window below
+          (split-window-vertically (floor (* 0.65 (window-height)))) ;; Split vertically
+          (other-window 1) ;; Move to the new window
+          ;; (+vterm/here-workspace nil) ;; Start vterm in the new window
+          (+vterm/get-or-create-for-workspace)
+          ;; (set-window-dedicated-p (selected-window) t) ;; Make dedicated! Maybe helps prevent resize
+          ))))
+
   (defun +vterm/split-and-here-workspace (arg)
     "Split window vertically, follow, and vterm!"
     (interactive "P")
@@ -38,7 +65,7 @@
     (+vterm/here-workspace arg)
     )
 
-  (defun +vterm/toggle-window (arg)
+  (defun +vterm/toggle-window-old (arg)
     "Toggles a terminal popup window at project root.
 
     If prefix ARG is non-nil, recreate vterm buffer in the current
@@ -53,7 +80,7 @@
            (
             ;; Get target buffer name, unique per window
             (target_buffer_name
-             (if (string-prefix-p "*doom:vterm-popup:" (buffer-name))
+             (if (string-prefix-p (+vterm/get-workspace-buffer-name) (buffer-name)) ;
                  (buffer-name)
                (format "*doom:vterm-popup:%s:%s*"
                        (if (bound-and-true-p persp-mode)
@@ -98,8 +125,8 @@
              ;; Split window vertically, follow, and display the buffer!
              (split-window-vertically (floor (* 0.65 (window-height))))
              (other-window 1)
-             (set-window-buffer (selected-window) buffer)
-             (set-window-dedicated-p (selected-window) t) ;; Make dedicated! Maybe helps prevent resize
+             ;; (set-window-buffer (selected-window) buffer)
+             ;; (set-window-dedicated-p (selected-window) t) ;; Make dedicated! Maybe helps prevent resize
              (set-window-parameter (selected-window) 'window-size-fixed 'height)
              )
            )
