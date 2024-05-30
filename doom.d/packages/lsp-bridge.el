02f01f5b69ed14415c6330d563d1365bcd070a85
Logan Grado <logan.grado@amd.com>
2024-05-30 16:15:24 -0500

    comment out lsp-bridge

--- doom.d/packages/lsp-bridge.el
+++ doom.d/packages/lsp-bridge.el
@@ -1,7 +1,7 @@
 ;;; ../.dotfiles/doom.d/packages/lsp-bridge.el -*- lexical-binding: t; -*-
 
 
-(use-package! lsp-bridge
-  :config
-  (setq lsp-bridge-enable-log nil)
-  (global-lsp-bridge-mode))
+;; (use-package! lsp-bridge
+;;   :config
+;;   (setq lsp-bridge-enable-log nil)
+;;   (global-lsp-bridge-mode))
