8864a47a40f44e624ec07b09930df3ea3f937619
Logan Grado <logan.grado@amd.com>
2024-06-05 16:48:10 -0500

    Clean up packages, comment out lsp-bridge

--- doom.d/packages/lsp-bridge.el
+++ doom.d/packages/lsp-bridge.el
@@ -1,7 +1,10 @@
 ;;; ../.dotfiles/doom.d/packages/lsp-bridge.el -*- lexical-binding: t; -*-
 
+;; LSP Bridge Mode: Some super fast auto-complete. Not sure if this works literally everywhere
+;; https://github.com/manateelazycat/lsp-bridge
+;; Make sure to install deps with `pip3'
 
-(use-package! lsp-bridge
-  :config
-  (setq lsp-bridge-enable-log nil)
-  (global-lsp-bridge-mode))
+;; (use-package! lsp-bridge
+;;   :config
+;;   (setq lsp-bridge-enable-log nil)
+;;   (global-lsp-bridge-mode))
