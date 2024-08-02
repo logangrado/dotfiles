8ba10b68acb7f2c6819dafa9a97c0d90c743b92c
Logan Grado <logan.grado@amd.com>
2024-08-01 20:30:27 -0500

    aphelia: autoformat jsonnet

--- doom.d/packages/apheleia.el
+++ doom.d/packages/apheleia.el
@@ -5,4 +5,11 @@
   ;; (setq python-black-extra-args (list "-l 120"))
   (setf (alist-get 'black apheleia-formatters)
         '("black" "-l" "120" "-"))
+
+  (add-to-list 'apheleia-formatters
+               '(jsonnetfmt . ("jsonnetfmt" "-")))
+
+  ;; Associate .jsonnet files with jsonnetfmt
+  (add-to-list 'apheleia-mode-alist
+               '(jsonnet-mode . jsonnetfmt))
   )
