f782c43ffdf10626120a43a7cd258a78d54def57
Logan Grado <grado.logan@gmail.com>
2025-08-13 12:11:09 -0500

    apheleia: apply isort on save

--- doom.d/packages/apheleia.el
+++ doom.d/packages/apheleia.el
@@ -2,14 +2,19 @@
 
 (use-package apheleia
   :config
+  ;; Add/configure formatters
+  ;; Use setf to modify the existing element
   ;; (setq python-black-extra-args (list "-l 120"))
   (setf (alist-get 'black apheleia-formatters)
         '("black" "-l" "120" "-"))
+  (setf (alist-get 'isort apheleia-formatters)
+        '("isort" "--stdout" "--profile" "--black" "-"))
+  (setf (alist-get 'jsonnetfmt apheleia-formatters)
+        '("jsonnetfmt" "-"))
 
-  (add-to-list 'apheleia-formatters
-               '(jsonnetfmt . ("jsonnetfmt" "-")))
-
-  ;; Associate .jsonnet files with jsonnetfmt
+  ;; Associate formatters
   (add-to-list 'apheleia-mode-alist
-               '(jsonnet-mode . jsonnetfmt))
+               '(jsonnet-mode . jsonnetfmt)
+               '(python-mode . (isort black))
+               )
   )
