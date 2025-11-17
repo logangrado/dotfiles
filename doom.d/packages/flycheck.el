e68c8088290d13758c41095cdb5414bdb21fef84
Logan Grado <grado.logan@gmail.com>
2025-11-17 09:33:28 -0600

    flycheck config

--- doom.d/packages/flycheck.el
+++ doom.d/packages/flycheck.el
@@ -4,4 +4,13 @@
   :config
 
   (setq flycheck-flake8-maximum-line-length 120)
+
+  (map! :map evil-normal-state-map
+        :prefix ("e" . "flycheck")
+        "j" #'flycheck-next-error
+        "k" #'flycheck-previous-error
+        "e" #'flycheck-explain-error-at-point
+        "l" #'flycheck-list-errors
+        )
+
   )
