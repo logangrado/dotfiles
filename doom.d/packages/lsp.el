0355469f461415c7c73eaf4a5bfbbce29ec135e5
Logan Grado <grado.logan@gmail.com>
2025-11-17 09:27:58 -0600

    LSP: Configure for basedpyright/ty, use basedpyright for now

--- doom.d/packages/lsp.el
+++ doom.d/packages/lsp.el
@@ -5,20 +5,40 @@
   :config
   (lsp-headerline-breadcrumb-mode)
   (setq
-    lsp-enable-symbol-highlighting t                    ;; Highlight symbol at point
-    lsp-ui-doc-enable t                                 ;; Docs on hover
-    lsp-lens-enable t                                   ;; Lens? Number of references?
-    lsp-ui-sideline-enable t                            ;; Enable sideline info
-    lsp-ui-sideline-show-code-actions nil               ;; Show code actions in sideline
-    lsp-ui-sideline-show-hover nil                      ;; Sideline hover
-    lsp-ui-sideline-show-diagnostics t                  ;; Diagnostics (errors) in sideline
-    lsp-eldoc-enable-hover t                            ;; Docs on hover (not sure what this is)
-    lsp-modeline-diagnostics-enable t                   ;; Enable modeline diagnostic
-    lsp-headerline-breadcrumb-enable t                  ;; Show breadcrumbs (project file path)
-    lsp-headerline-breadcrumb-enable-diagnostics nil    ;; Flycheck breadcrumbs
-    lsp-signature-auto-activate t                       ;; Show function signature
-    lsp-signature-render-documentation nil              ;; Show full documentation
-    lsp-completion-show-detail t                        ;; Detail in completion
-    lsp-completion-show-kind t                          ;; Kind (class, function) in completion
+   lsp-enable-symbol-highlighting t                    ;; Highlight symbol at point
+   lsp-ui-doc-enable t                                 ;; Docs on hover
+   lsp-lens-enable t                                   ;; Lens? Number of references?
+   lsp-ui-sideline-enable t                            ;; Enable sideline info
+   lsp-ui-sideline-show-code-actions nil               ;; Show code actions in sideline
+   lsp-ui-sideline-show-hover nil                      ;; Sideline hover
+   lsp-ui-sideline-show-diagnostics t                  ;; Diagnostics (errors) in sideline
+   lsp-eldoc-enable-hover t                            ;; Docs on hover (not sure what this is)
+   lsp-modeline-diagnostics-enable t                   ;; Enable modeline diagnostic
+   lsp-headerline-breadcrumb-enable t                  ;; Show breadcrumbs (project file path)
+   lsp-headerline-breadcrumb-enable-diagnostics nil    ;; Flycheck breadcrumbs
+   lsp-signature-auto-activate t                       ;; Show function signature
+   lsp-signature-render-documentation nil              ;; Show full documentation
+   lsp-completion-show-detail t                        ;; Detail in completion
+   lsp-completion-show-kind t                          ;; Kind (class, function) in completion
    )
+  (setq
+   ;; Ensure LSP spanws one project per root
+   lsp-keep-workspace-alive nil ;; Don't keep workspaces alive when switching projects
+   lsp-auto-guess-root t ;; Better guessing of project root
+   )
+
+
+  ;; (setq lsp-pyright-langserver-command '("uvx" "basedpyright"))
+  (setq lsp-pyright-langserver-command "basedpyright")
+  (setq lsp-python-ty-clients-server-command '("uvx" "ty" "server"))
+
+  ;; comment out the server we dont want
+  (setq lsp-disabled-clients '
+        (
+         ty-ls
+         ;; pyright
+         ))
+
+  ;; (optional) auto-refresh when switching projects
+  (add-hook 'projectile-after-switch-project-hook #'lsp-restart-workspace)
   )
