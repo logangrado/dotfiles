fcf2f8e65d14402695b433678a0524d94a0a0887
Logan Grado <grado.logan@gmail.com>
2025-11-17 09:41:57 -0600

    centaur-tabs: Attempting better ordering for vterm buffers

--- doom.d/packages/centaur-tabs.el
+++ doom.d/packages/centaur-tabs.el
@@ -18,6 +18,20 @@
   ;; :hook
   ;; ;; Disalbe tabs in vterm
   ;; ((vterm-mode vterm-toggle--mode) . centaur-tabs-local-mode)
+  :init
+  ;; Parse *v:NAME and *v:NAME<N>; base (no <N>) is index 0
+  (defun lg/vterm-index (buf)
+    "Return numeric index for vterm buffers:
+   *v:workspace  -> 0
+   *v:workspace<1> -> 1
+   Non-vterm buffers -> nil (don’t sort)."
+    (with-current-buffer buf
+      (when (eq major-mode 'vterm-mode)
+        (let ((name (buffer-name buf)))
+          (when (string-match "\\`\\*v:[^<]+\\(?:<\\([0-9]+\\)>\\)?\\'" name)
+            (let ((m (match-string 1 name)))
+              (if m (string-to-number m) 0)))))))
+
 
   :config
   (centaur-tabs-mode t)
@@ -36,7 +50,7 @@
 
   ;; CUSTOM TABS GROUPING
   ;; Based off default centaur-tabs-buffer-group
-  (defun my/centaur-tabs-buffer-groups ()
+  (defun lg/centaur-tabs-buffer-groups ()
     "`centaur-tabs-buffer-groups' control buffers' group rules.
 
         Group centaur-tabs with mode if buffer is derived from `eshell-mode'
@@ -54,14 +68,14 @@
            ;; Fallback group name for vterm buffers not in a project
            "vterm")))
       ((or (string-equal "*" (substring (buffer-name) 0 1))
-	   (memq major-mode '(magit-process-mode
-			      magit-status-mode
-			      magit-diff-mode
-			      magit-log-mode
-			      magit-file-mode
-			      magit-blob-mode
-			      magit-blame-mode
-			      )))
+           (memq major-mode '(magit-process-mode
+                              magit-status-mode
+                              magit-diff-mode
+                              magit-log-mode
+                              magit-file-mode
+                              magit-blob-mode
+                              magit-blame-mode
+                              )))
        "Emacs")
       ((derived-mode-p 'eshell-mode)
        "EShell")
@@ -75,19 +89,36 @@
        (centaur-tabs-get-group-name (current-buffer))))))
 
   ;; Set the custom grouping function
-  (setq centaur-tabs-buffer-groups-function 'my/centaur-tabs-buffer-groups)
+  (setq centaur-tabs-buffer-groups-function 'lg/centaur-tabs-buffer-groups)
 
-  ;; Make centaur-tabs use the same buffer list as persp mode
-  (defun my/centaur-tabs-buffer-list ()
-    "Return buffers visible in the current workspace."
-    (let ((buffers (persp-buffer-list)))
-      (cl-remove-if
-       (lambda (buffer)
-         (or (string-prefix-p " " (buffer-name buffer)) ; internal buffers
-             ;; (string-prefix-p "*" (buffer-name buffer)) ; hidden buffers
-             (not (buffer-live-p buffer))))
-       buffers)))
-  (setq centaur-tabs-buffer-list-function #'my/centaur-tabs-buffer-list)
+  ;; ;; Make centaur-tabs use the same buffer list as persp mode
+  ;; (defun lg/centaur-tabs-buffer-list ()
+  ;;   "Return buffers visible in the current workspace."
+  ;;   (let ((buffers (persp-buffer-list)))
+  ;;     (cl-remove-if
+  ;;      (lambda (buffer)
+  ;;        (or (string-prefix-p " " (buffer-name buffer)) ; internal buffers
+  ;;            ;; (string-prefix-p "*" (buffer-name buffer)) ; hidden buffers
+  ;;            (not (buffer-live-p buffer))))
+  ;;      buffers)))
+  ;; (setq centaur-tabs-buffer-list-function #'lg/centaur-tabs-buffer-list)
+  (defun lg/centaur-tabs-buffer-list ()
+    "Buffers visible in current workspace, with *v:workspace<N> sorted by N."
+    (let* ((raw (persp-buffer-list))
+           (buffers (cl-remove-if
+                     (lambda (b)
+                       (or (string-prefix-p " " (buffer-name b))
+                           (not (buffer-live-p b))))
+                     raw)))
+      ;; stable-sort: vterm by numeric index; others keep existing order
+      (cl-stable-sort
+       buffers
+       (lambda (a b)
+         (let ((ia (lg/vterm-index a))
+               (ib (lg/vterm-index b)))
+           (and ia ib (< ia ib)))))))
+
+  (setq centaur-tabs-buffer-list-function #'lg/centaur-tabs-buffer-list)
 
   (add-hook 'persp-switch-hook #'centaur-tabs-headline-match)
   ;; Update tabs on switching perspectives (workspaces)
