6a3aeee6a19ec89572d5b5948a56f54a53489bc4
Logan Grado <grado.logan@gmail.com>
2025-11-19 14:54:27 -0600

    treesitter: better python folding

--- doom.d/packages/treesitter.el
+++ doom.d/packages/treesitter.el
@@ -38,15 +38,46 @@
   ;;       (when (>= (point) here)
   ;;         (goto-char here)))))
 
+  ;; DEFAULT FOLDING FOR PYTHON:
+  ;; (python-ts-mode (import_statement . treesit-fold-range-seq)
+  ;; (import_from_statement . treesit-fold-range-seq)
+  ;; (future_import_statement . treesit-fold-range-seq)
+  ;; (function_definition . treesit-fold-range-python-def)
+  ;; (class_definition . treesit-fold-range-python-def)
+  ;; (while_statement . treesit-fold-range-python-block)
+  ;; (for_statement . treesit-fold-range-python-block)
+  ;; (if_statement . treesit-fold-range-python-block)
+  ;; (elif_clause . treesit-fold-range-python-block)
+  ;; (else_clause . treesit-fold-range-python-block)
+  ;; (match_statement . treesit-fold-range-python-block)
+  ;; (case_clause . treesit-fold-range-python-block)
+  ;; (try_statement . treesit-fold-range-python-block)
+  ;; (except_clause . treesit-fold-range-python-block)
+  ;; (with_statement . treesit-fold-range-python-block)
+  ;; (list . treesit-fold-range-seq)
+  ;; (dictionary . treesit-fold-range-seq)
+  ;; (parenthesized_expression . treesit-fold-range-seq)
+  ;; (expression_statement
+  ;;  . treesit-fold-range-python-expression-statement)
+  ;; (comment lambda (node offset)
+  ;;          (treesit-fold-range-line-comment node offset "#")))
+  (setf (alist-get 'python-ts-mode treesit-fold-range-alist)
+        '((class_definition    . treesit-fold-range-python-def)
+          (function_definition . treesit-fold-range-python-def)
+          ;; optional: fold blocks of `#` comments
+          (comment . (lambda (node offset)
+                       (treesit-fold-range-line-comment node offset "#")))))
+
+  (setq treesit-fold-line-count-show nil)
+
   ;; Keybindings: mirror your Origami layout under evil normal state
   (map! :map evil-normal-state-map
         :prefix ("c" . "fold/treesit")
-        ;; "c" #'treesit-fold-toggle
-        "o" #'treesit-fold-open-recursively
-        ;; "o" #'treesit-fold-open
+        "t" #'treesit-fold-toggle
+        ;; "o" #'treesit-fold-open-recursively
+        "o" #'treesit-fold-open
         "O" #'treesit-fold-open-all
         "c" #'treesit-fold-close
         "C" #'treesit-fold-close-all
         )
   )
-
