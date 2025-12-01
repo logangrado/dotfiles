5b500356b2150e3f7e9e6afe510b910c86c97ff3
Logan Grado <grado.logan@gmail.com>
2025-12-01 10:20:41 -0600

    org-roam: Update custom func names

--- doom.d/packages/org-roam.el
+++ doom.d/packages/org-roam.el
@@ -19,7 +19,7 @@
   ;; This series of functions helps for org agenda
   ;; They will dynamically add/remove the project tag from org roam files
   ;; Based on this blog: https://d12frosted.io/posts/2021-01-16-task-management-with-roam-vol5.html
-  (defun vulpea-project-p ()
+  (defun lg/vulpea-project-p ()
     "Return non-nil if current buffer has any todo entry.
         TODO entries marked as done are ignored, meaning the this
         function returns nil if current buffer contains only completed
@@ -32,17 +32,17 @@
             'todo))
       nil 'first-match))                     ; (3)
 
-  (defun vulpea-project-update-tag ()
+  (defun lg/vulpea-project-update-tag ()
+    "Update PROJECT tag in the current buffer."
     (interactive)
     (message "UPDATING VULPEA PROJECT TAG")
-    "Update PROJECT tag in the current buffer."
     (when (and (not (active-minibuffer-window))
-               (vulpea-buffer-p))
+               (lg/vulpea-buffer-p))
       (save-excursion
         (goto-char (point-min))
         (let* ((tags (vulpea-buffer-tags-get))
                (original-tags tags))
-          (if (vulpea-project-p)
+          (if (lg/vulpea-project-p)
               (setq tags (cons "project" tags))
             (setq tags (remove "project" tags)))
 
@@ -54,18 +54,18 @@
                     (seq-difference original-tags tags))
             (apply #'vulpea-buffer-tags-set tags))))))
 
-  (defun vulpea-buffer-p ()
+  (defun lg/vulpea-buffer-p ()
     "Return non-nil if the currently visited buffer is a note."
     (and buffer-file-name
          (string-prefix-p
           (expand-file-name (file-name-as-directory org-roam-directory))
           (file-name-directory buffer-file-name))))
 
-  (add-hook 'find-file-hook #'vulpea-project-update-tag)
-  (add-hook 'before-save-hook #'vulpea-project-update-tag)
+  (add-hook 'find-file-hook #'lg/vulpea-project-update-tag)
+  (add-hook 'before-save-hook #'lg/vulpea-project-update-tag)
 
   ;; Functions for BUILDING THE AGENDA
-  (defun vulpea-project-files ()
+  (defun lg/vulpea-project-files ()
     "Return a list of note files containing 'project' tag." ;
     (seq-uniq
      (seq-map
@@ -77,26 +77,26 @@
         :on (= tags:node-id nodes:id)
         :where (like tag (quote "%\"project\"%"))]))))
 
-  (defun vulpea-agenda-files-update (&rest _)
+  (defun lg/vulpea-agenda-files-update (&rest _)
     "Update the value of `org-agenda-files'."
     (interactive)
-    (setq org-agenda-files (vulpea-project-files)))
+    (setq org-agenda-files (lg/vulpea-project-files)))
 
-  (advice-add 'org-agenda :before #'vulpea-agenda-files-update)
-  (advice-add 'org-todo-list :before #'vulpea-agenda-files-update)
+  (advice-add 'org-agenda :before #'lg/vulpea-agenda-files-update)
+  (advice-add 'org-todo-list :before #'lg/vulpea-agenda-files-update)
 
-  (defun vulpea-update-all-project-states ()
+  (defun lg/vulpea-update-all-project-states ()
     (interactive)
     (dolist (file (org-roam-list-files))
       (message "processing %s" file)
       (with-current-buffer (or (find-buffer-visiting file)
                                (find-file-noselect file))
-        (vulpea-project-update-tag)
+        (lg/vulpea-project-update-tag)
         (save-buffer))))
   ;; END ORG ROAM AGENDA
   ;; ===================
 
-  (defun org-roam-search ()
+  (defun lg/org-roam-search ()
     "Search org-roam directory using consult-ripgrep. With live-preview."
     (interactive)
     (let ((consult-ripgrep-command "rg --multiline --null --ignore-case --type org --line-buffered --color=always --max-columns=500 --no-heading --line-number . -e ARG OPTS"))
@@ -104,15 +104,15 @@
 
   ;; Org roam weeklies
   ;;==========================================================================================
-  (defun org-roam-weeklies--file-path (time)
+  (defun lg/org-roam-weeklies--file-path (time)
     "Return the file path for the weekly note corresponding to TIME."
     (let ((filename (format-time-string "weekly/%Y-W%V.org" time)))
       (expand-file-name filename org-roam-directory)))
 
-  (defun org-roam-weeklies-goto-today ()
+  (defun lg/org-roam-weeklies-goto-today ()
     "Find or create the weekly note for the current week."
     (interactive)
-    (let ((file-path (org-roam-weeklies--file-path (current-time))))
+    (let ((file-path (lg/org-roam-weeklies--file-path (current-time))))
       (find-file file-path)
       (unless (file-exists-p file-path)
         (insert (format "#+title: Week %s, %s\n\n"
@@ -120,7 +120,7 @@
                         (format-time-string "%Y")))
         (save-buffer))))
 
-  (defun org-roam-weeklies--get-week-from-file ()
+  (defun lg/org-roam-weeklies--get-week-from-file ()
     "Extract the year and week number from current weekly note filename.
 Returns a cons cell (YEAR . WEEK) or nil if not in a weekly note."
     (when buffer-file-name
@@ -129,7 +129,7 @@ Returns a cons cell (YEAR . WEEK) or nil if not in a weekly note."
         (cons (string-to-number (match-string 1 buffer-file-name))
               (string-to-number (match-string 2 buffer-file-name))))))
 
-  (defun org-roam-weeklies--time-for-week (year week offset)
+  (defun lg/org-roam-weeklies--time-for-week (year week offset)
     "Create a time value for YEAR and WEEK with OFFSET weeks added."
     ;; Convert to time using first day of the week (Monday)
     (let* ((time (encode-time 0 0 0 1 1 year))
@@ -139,16 +139,16 @@ Returns a cons cell (YEAR . WEEK) or nil if not in a weekly note."
            (offset-seconds (* seconds-per-week (1- target-week))))
       (time-add time offset-seconds)))
 
-  (defun org-roam-weeklies-goto-next ()
+  (defun lg/org-roam-weeklies-goto-next ()
     "Go to the next weekly note.
 If not in a weekly note, go to next week from current week."
     (interactive)
-    (let* ((week-info (or (org-roam-weeklies--get-week-from-file)
+    (let* ((week-info (or (lg/org-roam-weeklies--get-week-from-file)
                           (org-roam-weeklies--get-current-week)))
            (year (car week-info))
            (week (cdr week-info))
-           (next-time (org-roam-weeklies--time-for-week year week 1))
-           (file-path (org-roam-weeklies--file-path next-time)))
+           (next-time (lg/org-roam-weeklies--time-for-week year week 1))
+           (file-path (lg/org-roam-weeklies--file-path next-time)))
       (find-file file-path)
       (unless (file-exists-p file-path)
         (insert (format "#+title: Week %s, %s\n\n"
@@ -156,16 +156,16 @@ If not in a weekly note, go to next week from current week."
                         (format-time-string "%Y" next-time)))
         (save-buffer))))
 
-  (defun org-roam-weeklies-goto-previous ()
+  (defun lg/org-roam-weeklies-goto-previous ()
     "Go to the previous weekly note.
 If not in a weekly note, go to previous week from current week."
     (interactive)
-    (let* ((week-info (or (org-roam-weeklies--get-week-from-file)
+    (let* ((week-info (or (lg/org-roam-weeklies--get-week-from-file)
                           (org-roam-weeklies--get-current-week)))
            (year (car week-info))
            (week (cdr week-info))
-           (prev-time (org-roam-weeklies--time-for-week year week -1))
-           (file-path (org-roam-weeklies--file-path prev-time)))
+           (prev-time (lg/org-roam-weeklies--time-for-week year week -1))
+           (file-path (lg/org-roam-weeklies--file-path prev-time)))
       (find-file file-path)
       (unless (file-exists-p file-path)
         (insert (format "#+title: Week %s, %s\n\n"
@@ -189,7 +189,7 @@ If not in a weekly note, go to previous week from current week."
   :bind
   (("C-c n f" . org-roam-node-find)
    ("C-c n r" . org-roam-db-sync)
-   ("C-c n s" . org-roam-search)
+   ("C-c n s" . lg/org-roam-search)
    :map org-mode-map
    ("C-c n i" . org-roam-node-insert)
    ("C-c n o" . org-id-get-create)
@@ -200,9 +200,9 @@ If not in a weekly note, go to previous week from current week."
    ("Y" . org-roam-dailies-capture-yesterday)
    ("T" . org-roam-dailies-capture-tomorrow)
    :map org-roam-weeklies-map
-   ("w" . org-roam-weeklies-goto-today)
-   ("n" . org-roam-weeklies-goto-next)
-   ("p" . org-roam-weeklies-goto-previous)
+   ("w" . lg/org-roam-weeklies-goto-today)
+   ("n" . lg/org-roam-weeklies-goto-next)
+   ("p" . lg/org-roam-weeklies-goto-previous)
    )
 
 
