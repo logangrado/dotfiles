95062173de4ea8faed756f811193fa9f78f28395
Logan Grado <grado.logan@gmail.com>
2025-12-01 15:51:15 -0600

    forge: add example

--- doom.d/packages/forge-config.el
+++ doom.d/packages/forge-config.el
@@ -6,4 +6,11 @@
   :config
   (setq forge-add-pullreq-refspec nil)
 
+  ;; Example adding Forge source (do this for private hosts)
+  ;; (push '("gitlab.com"               ; GITHOST
+  ;;         "gitlab.com/api/v4"        ; APIHOST
+  ;;         "gitlab.com"               ; WEBHOST and INSTANCE-ID
+  ;;         forge-gitlab-repository)    ; CLASS
+  ;;       forge-alist)
+
   )
