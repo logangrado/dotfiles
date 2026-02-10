eab14c3eddb58082877a5a014a165bbdf7aed4c2
Logan Grado <grado.logan@gmail.com>
2026-02-10 09:43:18 -0600

    Company: update config

--- doom.d/packages/company.el
+++ doom.d/packages/company.el
@@ -2,6 +2,17 @@
 
 (use-package! company
   :bind
-  (:map company-active-map ("<return>" . newline))  ;; Make RET enter a newline
-  (:map company-active-map ("<tab>" . company-complete-selection)) ;; Use TAB for completion (instead of complete common)
+  (:map company-active-map
+        ("<return>" . newline)  ;; Make RET enter a newline
+        ("<tab>" . company-complete-selection)
+        ) ;; Use TAB for completion (instead of complete common)
+  :custom
+  (company-minimum-prefix-length 1)
+  (company-idle-delay 0)
+  (company-tooltip-idle-delay 0)
+
+  :config
+  (after! lsp-mode
+    (map! :map lsp-mode-map
+          "<tab>" #'company-indent-or-complete-common))
   )
